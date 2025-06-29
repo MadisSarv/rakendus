1f30ea924e9d2b78e4c2fc61311d9fd27a889c29
Mariam Reintop <mariam.reintop@tietoevry.com>
2025-06-29 19:05:03 +0300

    fix

--- app/(locations)/location.tsx
+++ app/(locations)/location.tsx
@@ -1,11 +1,16 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { View } from 'react-native';
+import { Text } from 'react-native-paper';
 
 const Location = () => {
   const router = useRouter();
 
-  return <View>Mine masina detailidesse</View>;
+  return (
+    <View>
+      <Text>Mine masina detailidesse</Text>
+    </View>
+  );
 };
 
 export default Location;
